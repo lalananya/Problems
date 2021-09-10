73ee6ae676eb52d7850b7074e826fb4d8b6a8eec
lalananya <86592595+lalananya@users.noreply.github.com>
2021-09-10 17:49:26 +0530

    tree-avl-improvements

--- Tree/avl-c.js
+++ Tree/avl-c.js
@@ -2,29 +2,39 @@ class Node{
     constructor(data,left=null,right=null){
         this.data = data;
         this.left = left;
-        this.right = right;
+        this.right = right;    
     }
 }
 
 class AVLTree{
     constructor(){
         this.root = null;
+        this.insertType = '';
     }
 
     insert(data){
         let newNode = new Node(data);
-        if(this.root === null) this.root = newNode;
+        if(this.root === null) {
+            this.root = newNode;
+            this.insertType += '';
+        }
         else{
             this.insertNode(this.root,newNode)
         }
     }
     insertNode(current,newNode){
        if(newNode.data < current.data){
-           if(current.left === null) current.left = newNode;
+           if(current.left === null) {
+            current.left = newNode;
+            this.insertType += 'L';
+           }
            else this.insertNode(current.left,newNode);
        }
        if(newNode.data > current.data){
-        if(current.right === null) current.right = newNode;
+        if(current.right === null) {
+            current.right = newNode;
+            this.insertType += 'R';
+        }
         else this.insertNode(current.right,newNode);
        }
     }
@@ -32,7 +42,6 @@ class AVLTree{
         if(current === null) return -1;
         let leftTree = this.heightofNode(current.left) + 1;
         let righTree = this.heightofNode(current.right) + 1;
-        console.log(`${current}, ${Math.max(leftTree,righTree)}`);
         return Math.max(leftTree,righTree);
     }
 
@@ -40,22 +49,43 @@ class AVLTree{
         if(current === null) return -1;
         let leftTree = this.heightofNode(current.left) + 1;
         let righTree = this.heightofNode(current.right) + 1;
-        console.log(`${current}, ${leftTree - righTree}`);
-        return Math.max(leftTree,righTree);
+        return ( leftTree - righTree < -1 || leftTree - righTree > 1) ? current : null ;
+    }
+
+    performLLRotation(current){
+        let temp = current;
+        this.root = current.left;
+        this.root.right = temp;
+    }
+
+    performRRRotation(current){
+        let temp = current;
+        this.root = current.right;
+        this.root.left = temp;
     }
     
 }
 
 (function run(){
     var avl = new AVLTree();
-    avl.insert(50);
-    avl.insert(40);
-    avl.insert(60);
-    avl.insert(55);
-    avl.insert(80);
-    avl.insert(90);
-    avl.insert(30);
-    console.log(avl.balanceFactor(avl.root));
+    //var arr = [50,40,60,55,80,90,30];
+    var arr = [50,40,30];
+    for(let i=0; i<arr.length; i++){
+        avl.insert(arr[i]);
+        let imbalancedNode = avl.balanceFactor(avl.root);
+        if(imbalancedNode !== null){
+            if(avl.insertType === 'LL'){
+                avl.performLLRotation(imbalancedNode);
+                console.log(avl);
+            }
+            if(avl.insertType === 'RR'){
+                avl.performRRRotation(imbalancedNode);
+                console.log(avl);
+            }
+        }
+    }
 })();
+
+/* Needs more logic and improvement overall */
 
 
