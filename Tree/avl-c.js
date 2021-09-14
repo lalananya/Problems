5f2f80ac125a7561014661ac8bf249e75c71c158
lalananya <86592595+lalananya@users.noreply.github.com>
2021-09-14 10:17:47 +0530

    avl-rotation

--- Tree/avl-c.js
+++ Tree/avl-c.js
@@ -63,13 +63,27 @@ class AVLTree{
         this.root = current.right;
         this.root.left = temp;
     }
+
+    performLRRotation(current){
+        let temp = current.left;
+        current.left = temp.right;
+        current.left.left = temp;
+        this.performLLRotation(current);
+    }
+
+    performRLRotation(current){
+        let temp = current.right;
+        current.right = temp.left;
+        current.right.right = temp;
+        this.performRRRotation(current);
+    }
     
 }
 
 (function run(){
     var avl = new AVLTree();
     //var arr = [50,40,60,55,80,90,30];
-    var arr = [50,40,30];
+    var arr = [50,60,55];
     for(let i=0; i<arr.length; i++){
         avl.insert(arr[i]);
         let imbalancedNode = avl.balanceFactor(avl.root);
@@ -80,6 +94,14 @@ class AVLTree{
             }
             if(avl.insertType === 'RR'){
                 avl.performRRRotation(imbalancedNode);
+                console.log(avl);
+            }
+            if(avl.insertType === 'LR'){
+                avl.performLRRotation(imbalancedNode);
+                console.log(avl);
+            }
+            if(avl.insertType === 'RL'){
+                avl.performRLRotation(imbalancedNode);
                 console.log(avl);
             }
         }
