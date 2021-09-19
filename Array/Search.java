a271203898088a315a74f86787a69d710286e664
lalananya <86592595+lalananya@users.noreply.github.com>
2021-09-19 11:35:30 +0530

    java

---
+++ Array/Search.java
@@ -0,0 +1,25 @@
+/* Following are the three known searching methods when data are stored in an array
+   Sequential Search
+   Binary Search
+   Interpolation Search
+
+   Write a geeric implementation for the above searching methods so that they can be used with any type of data
+   stored in the array
+*/
+
+/*---------------JUST FYI------------------*/
+class GenericClass<T>{ // T is used for generics in java, this will be used when you need to deal with differnt data types
+
+   T obj;
+   // SearchingClass(SearchinClass obj){
+   //    this.obj = obj;
+   // }
+   public T getObject() {return this;}
+}
+/*----------------------------------------- */
+
+
+class Search{
+
+
+}
